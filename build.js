7cf13dee69a9939c70b641236dea4c5c4235059e
Eric Dingle <eric.dingle@gmail.com>
2018-02-25 21:34:11 -0500

    Use uglify-es to further minify.

--- build.js
+++ build.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
-const minify = require('html-minifier').minify;
+const htmlMinify = require('html-minifier').minify;
 const parse5 = require('parse5');
 const Bundler = require('polymer-bundler').Bundler;
+const jsMinify = require('uglify-es').minify;
 
 const bundler = new Bundler({stripComments: true});
+
+function minifyJS(text, inline) {
+  const result = jsMinify(text, {parse: {bare_returns: inline}});
+  if (result.error) {
+    console.log(result.error);
+    return text;
+  }
+  return result.code;
+}
 
 (async function() {
   const manifest = await bundler.generateManifest(['index_dev.html']);
@@ -11,10 +21,10 @@ const bundler = new Bundler({stripComments: true});
 
   let out = parse5.serialize(bundle.documents.get('index_dev.html').ast);
 
-  out = minify(out, {
+  out = htmlMinify(out, {
     collapseWhitespace: true,
     minifyCSS: true,
-    minifyJS: true,
+    minifyJS: minifyJS,
     removeComments: true,
     sortAttributes: true,
     sortClassName: true
